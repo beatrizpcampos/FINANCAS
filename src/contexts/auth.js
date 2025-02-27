5c78454dafd33ec8cff55e642273aac3ee9faac6
beatrizpcampos <beatrizcampos300@gmail.com>
2025-02-27 16:00:36 -0300

    connection to database and requirements

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,14 +1,37 @@
 import { createContext, useState } from "react";
+import api from '../services/api'
+import { useNavigation } from "@react-navigation/native";
 
 export const AuthContext = createContext({})
 
 export default function AuthProvider({ children }){
-    const [ user, setUser ] = useState({
-        nome: 'beatriz'
-    })
+    const [ user, setUser ] = useState(null)
+    const [ loading, setLoading ] = useState(false)
+
+    const navigation = useNavigation()
+
+    async function signUp(nome, email, password) {
+        setLoading(true)
+
+        try{
+            const response = await api.post('/users', {
+                name: nome,
+                email: email,
+                password: password
+            })
+
+            setLoading(false)
+
+            navigation.goBack()
+
+        }catch(err){
+            console.log('erro ao cadastrar', err)
+            setLoading(false)
+        }
+    }
 
     return(
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ user, signUp, loading }}>
             { children }
         </AuthContext.Provider>
     )
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,31 @@
-import { Platform } from 'react-native'
+import { ActivityIndicator, Platform } from 'react-native'
 import {
-    Background, 
-    AreaInput, 
+    Background,
+    AreaInput,
     Input,
     SubmitButton,
     SubmitText
 } from '../SignIn/styles'
 import { useNavigation } from '@react-navigation/native'
 import styled from 'styled-components/native'
+import { useContext, useState } from 'react'
+import { AuthContext } from '../../contexts/auth'
 
 export default function SignUp() {
-    const navigation = useNavigation()
+    const { signUp, loading } = useContext(AuthContext)
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSignUp() {
+        if(nome === '' || email === '' || password === '') return
+        
+        signUp(nome, email, password)
+    }
 
     return (
         <Background>
-            <Container 
+            <Container
                 behavior={Platform.OS === 'ios' ? 'paddiing' : ''}
                 enabled
             >
@@ -22,23 +33,36 @@ export default function SignUp() {
                 <AreaInput>
                     <Input
                         placeholder='Digite seu nome'
+                        value={nome}
+                        onChangeText={(text) => setNome(text)}
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input
                         placeholder='Digite seu email'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input
                         placeholder='Digite sua senha'
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                        secureTextEntry={true}
                     />
                 </AreaInput>
 
-                <SubmitButton activeOpacity={0.8}>
-                    <SubmitText>Cadastrar</SubmitText>
+                <SubmitButton activeOpacity={0.8} onPress={handleSignUp}>
+                    {
+                        loading ? (
+                            <ActivityIndicator size={20} color='#fff' />
+                        ) : (
+                            <SubmitText>Cadastrar</SubmitText>
+                        )
+                    }
                 </SubmitButton>
 
             </Container>
